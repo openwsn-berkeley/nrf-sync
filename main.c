896ea53b5442ef6982d769c534b3961dc5fabcf4
Martina Balbi <martina.balbi14@gmail.com>
2021-04-21 16:13:07 +0200

    Project skeleton

---
+++ main.c
@@ -0,0 +1,24 @@
+
+/** @file
+*
+* @defgroup nrf-sync_main main.c
+* @{
+* @ingroup nrf-sync
+* @brief nrf-sync Application main file.
+*
+* This file contains the source code for nrf-sync.
+*/
+
+#include <stdbool.h>
+#include <stdint.h>s
+
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
+{
+    
+}
+
+
+/** @} */
