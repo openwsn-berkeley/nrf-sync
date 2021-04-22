93f8aee1d53de8fe49fbbf3e777b950711506f75
Martina Balbi <martina.balbi14@gmail.com>
2021-04-22 14:39:46 +0200

    cambio

--- nrf-sync_transmitter/main.c
+++ nrf-sync_transmitter/main.c
@@ -1,10 +1,9 @@
-
 /** @file
 *
-* @defgroup nrf-sync_transmitter_main main.c
+* @defgroup nrf-sync_receiver_main main.c
 * @{
-* @ingroup nrf-sync_transmitter
-* @brief Nrf-Sync Transmitter Application main file.
+* @ingroup nrf-sync_receiver
+* @brief Nrf-Sync Receiver Application main file.
 *
 * This file contains the source code for the Nrf-Sync application.
 *
@@ -15,160 +14,75 @@
 #include <stdio.h>
 #include "radio_config.h"
 #include "nrf_gpio.h"
-#include "app_timer.h"
 #include "boards.h"
 #include "bsp.h"
+#include "app_timer.h"
 #include "nordic_common.h"
 #include "nrf_error.h"
+#include "app_error.h"
 
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
 
-static uint32_t                   packet;                    /**< Packet to transmit. */
+//GPIOTE info
+#define OUTPUT_PIN_NUMBER 8UL //Output pin number
+#define OUTPUT_PIN_PORT 1UL //Output pin port
 
-/**@brief Function for sending packet.
+#define GPIOTE_CH 0
+
+//TIMER info
+#define PULSE_DURATION 10 //Time in ms
+#define PULSE_PERIOD 1000 //Time in ms -> 1 pulse per second
+
+#define TIMER_OFFSET 0 //Time in ms
+
+/**
+ * @brief Function for initializing output pin with GPIOTE. It will be set in Task mode with action on pin configured 
+ * to toggle. Event is generated when the pin toggles. Pin is set to begin low. 
  */
-void send_packet()
-{
-    // send the packet:
-    NRF_RADIO->EVENTS_READY = 0U;
-    NRF_RADIO->TASKS_TXEN   = 1;
-
-    while (NRF_RADIO->EVENTS_READY == 0U)
-    {
-        // wait
-    }
-    NRF_RADIO->EVENTS_END  = 0U;
-    NRF_RADIO->TASKS_START = 1U;
-
-    while (NRF_RADIO->EVENTS_END == 0U)
-    {
-        // wait
-    }
-
-    uint32_t err_code = bsp_indication_set(BSP_INDICATE_SENT_OK);
-    NRF_LOG_INFO("The packet was sent");
-    APP_ERROR_CHECK(err_code);
-
-    NRF_RADIO->EVENTS_DISABLED = 0U;
-    // Disable radio
-    NRF_RADIO->TASKS_DISABLE = 1U;
-
-    while (NRF_RADIO->EVENTS_DISABLED == 0U)
-    {
-        // wait
-    }
+void gpiote_setup() {
+    NRF_GPIOTE->CONFIG[GPIOTE_CH] = (GPIOTE_CONFIG_MODE_Task       << GPIOTE_CONFIG_MODE_Pos) |
+                                    (OUTPUT_PIN_NUMBER             << GPIOTE_CONFIG_PSEL_Pos) |
+                                    (OUTPUT_PIN_PORT               << GPIOTE_CONFIG_PORT_Pos) |
+                                    (GPIOTE_CONFIG_POLARITY_Toggle << GPIOTE_CONFIG_POLARITY_Pos) |
+                                    (GPIOTE_CONFIG_OUTINIT_Low     << GPIOTE_CONFIG_OUTINIT_Pos);
 }
 
-
-/**@brief Function for handling bsp events.
+/**
+ * @brief Function for initializing TIMER0. This Timer will be in charge of managing the pulse duration and frequency.
+ * Default values: PRESCALER = 4, MODE = Timer
  */
-void bsp_evt_handler(bsp_event_t evt)
-{
-    uint32_t prep_packet = 0;
-    switch (evt)
-    {
-        case BSP_EVENT_KEY_0:
-            /* Fall through. */
-        case BSP_EVENT_KEY_1:
-            /* Fall through. */
-        case BSP_EVENT_KEY_2:
-            /* Fall through. */
-        case BSP_EVENT_KEY_3:
-            /* Fall through. */
-        case BSP_EVENT_KEY_4:
-            /* Fall through. */
-        case BSP_EVENT_KEY_5:
-            /* Fall through. */
-        case BSP_EVENT_KEY_6:
-            /* Fall through. */
-        case BSP_EVENT_KEY_7:
-            /* Get actual button state. */
-            for (int i = 0; i < BUTTONS_NUMBER; i++)
-            {
-                prep_packet |= (bsp_board_button_state_get(i) ? (1 << i) : 0);
-            }
-            break;
-        default:
-            /* No implementation needed. */
-            break;
-    }
-    packet = prep_packet;
+void timer0_setup() {
+    NRF_TIMER0->BITMODE = TIMER_BITMODE_BITMODE_32Bit;
+
+    NRF_TIMER0->CC[0] = TIMER_OFFSET * 1000; //Timer offset trigger event to set gpio pin high
+    NRF_TIMER0->CC[1] = (TIMER_OFFSET + PULSE_DURATION) * 1000; //Timer trigger event to set gpio pin low, end of pulse
+    NRF_TIMER0->CC[2] = (PULSE_PERIOD * 1000); //End of Timer
+
+    //Event when CC[0] and CC[1] will be connected via PPI to the GPIOTE task. Event when CC[2] is shortcutted to clear timer 
+    //task. Then timer automatically starts again. This will be changed when the Radio is the one that manages this. 
+
+    NRF_TIMER0->SHORTS = TIMER_SHORTS_COMPARE2_CLEAR_Enabled << TIMER_SHORTS_COMPARE2_CLEAR_Pos;
 }
 
-
-/**@brief Function for initialization oscillators.
+/**
+ * @brief Function for initializing PPI. 
+ * Connections to be made: - EVENTS_COMPARE[0] with TASK_OUT[GPIOTE_CH] (will set pin high)
+ *                         - EVENTS_COMPARE[1] with TASK_OUT[GPIOTE_CH] (will set pin low)
  */
-void clock_initialization()
-{
-    /* Start 16 MHz crystal oscillator */
-    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
-    NRF_CLOCK->TASKS_HFCLKSTART    = 1;
-
-    /* Wait for the external oscillator to start up */
-    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0)
-    {
-        // Do nothing.
-    }
-
-    /* Start low frequency crystal oscillator for app_timer(used by bsp)*/
-    NRF_CLOCK->LFCLKSRC            = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
-    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
-    NRF_CLOCK->TASKS_LFCLKSTART    = 1;
-
-    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0)
-    {
-        // Do nothing.
-    }
+void ppi_setup() {
+    
 }
-
 
 /**
  * @brief Function for application main entry.
  * @return 0. int return type required by ANSI/ISO standard.
  */
-int main(void)
-{
-    uint32_t err_code = NRF_SUCCESS;
+int main(void) {
+    //TEST 1: Generate square wave with 10ms duty cycle, offset 0.
 
-    clock_initialization();
-
-    err_code = app_timer_init();
-    APP_ERROR_CHECK(err_code);
-
-    err_code = NRF_LOG_INIT(NULL);
-    APP_ERROR_CHECK(err_code);
-
-    NRF_LOG_DEFAULT_BACKENDS_INIT();
-
-    err_code = bsp_init(BSP_INIT_LEDS | BSP_INIT_BUTTONS, bsp_evt_handler);
-    APP_ERROR_CHECK(err_code);
-
-    // Set radio configuration parameters
-    radio_configure();
-
-    // Set payload pointer
-    NRF_RADIO->PACKETPTR = (uint32_t)&packet;
-
-    err_code = bsp_indication_set(BSP_INDICATE_USER_STATE_OFF);
-    NRF_LOG_INFO("Radio transmitter example started.");
-    NRF_LOG_INFO("Press Any Button");
-    APP_ERROR_CHECK(err_code);
-
-    while (true)
-    {
-        if (packet != 0)
-        {
-            send_packet();
-            NRF_LOG_INFO("The contents of the package was %u", (unsigned int)packet);
-            packet = 0;
-        }
-        NRF_LOG_FLUSH();
-        __WFE();
-    }
 }
-
 
 /**
  *@}
